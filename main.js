b77e7c5cbc0e9aa4e7af8255c14093a3e2943923
Chris <chrisvass1@gmail.com>
2017-12-08 09:56:02 -0500

    wow

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
+var c = document.getElementById('canvas');
+var ctx = c.getContext('2d');
+
 var alpha = document.getElementById("alpha");
 var beta = document.getElementById("beta");
 var gamma = document.getElementById("gamma");
 var absolute = document.getElementById("absolute");
-
+var a = 0;
+var b = 0;
+var g = 0;
 
 window.addEventListener("deviceorientation", function (e) {
    alpha.innerHTML = "Alpha: " + Math.round(e.alpha);
    beta.innerHTML = "Beta: " + Math.round(e.beta);
    gamma.innerHTML = "Gamma: " + Math.round(e.gamma);
    absolute.innerHTML = "Absolute: " + e.absolute;
+   a = e.alpha;
+   b = e.beta;
+   g = e.gamma;
+
  }, false);
+
+
+var x = c.width/2;
+var y = c.width/2;
+
+
+setInterval(function () {
+  x+=g/100;
+  y+=b/100;
+  ctx.clearRect(0,0,c.width,c.height);
+  ctx.fillRect(x,y,20,20);
+  console.log('fdf');
+},10)
